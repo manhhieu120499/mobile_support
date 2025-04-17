c93ac24e7c9c8fc692b2a4212f57ddd57e1e8b3f
manhhieu120499 <vomanhhieu62@gmail.com>
2025-04-17 21:37:50 +0700

    update

--- src/utilities/axiosConfig.js
+++ src/utilities/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosConfig = (token) => {
   return axios.create({
-    baseURL: "http://172.20.74.80:8080",
+    baseURL: "http://192.168.0.111:8080",
     timeout: 5000,
     headers: {
       "Content-Type": "application/json",
